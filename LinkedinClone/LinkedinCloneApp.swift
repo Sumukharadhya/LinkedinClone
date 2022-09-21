44a747efdd55c320122922415383114110b428eb
Sumukh <sumukharadhyakj07@gmail.com>
2022-09-22 00:14:20 +0530

    Initial Commit

---
+++ LinkedinClone/LinkedinCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkedinCloneApp.swift
+//  LinkedinClone
+//
+//  Created by Sumukh Kj on 22/09/22.
+//
+
+import SwiftUI
+
+@main
+struct LinkedinCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
